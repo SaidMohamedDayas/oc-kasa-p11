92c03368ecf84717cc8958fd2244a235bbe7216f
SaidMohamedDayas <113367982+SaidMohamedDayas@users.noreply.github.com>
2024-10-09 19:34:05 +0200

    dependencies: Add react-router-dom to project

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Welcome to the about page!</p>
+      <a href="/">Home</a>
+    </div>
+  );
+}
+
+export default About;
